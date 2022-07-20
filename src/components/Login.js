a1022f4e64a9ade243b48ce0001e3fc97ea6f9cf
Aman Agrawal <amanagrawal6828@gmail.com>
2022-07-20 20:00:43 +0530

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
+      <div className="my-5">
+        <h1 className="text-center display-6 fw-bolder">Login</h1>
+      </div>
+
+      <div className="container contact_div">
+        <div className="row">
+          <div className="col-md-6 col-10 mx-auto">
+            <form>
+              <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">
+                  User Name
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="exampleFormControlInput1"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="exampleFormControlInput1"
+                />
+              </div>
+              <div class="col-12">
+                <button class="btn btn-primary" type="submit">
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
